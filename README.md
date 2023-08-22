71f9df9f605876b71d9f5bddf4ad2418635d0ae1
yul402 <Ying.li20@outlook.com>
2023-08-21 20:17:43 -0500

    Updaated index file to address Dry principle

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # SVG-Logo-Maker
+![GitHub license](https://img.shields.io/badge/license-MIT-yellowgreen.svg)
+
+## Description
+
+This is a Node.js command-line application that takes in user input to generate a logo and save it as an SVG file. The application prompts the user to select a color and shape, provide text for the logo, and save the generated SVG to a .svg file.
+
+## Table of Contents
+
+* [Installation](#Installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+
+## Installation
+
+To install dependencies, run these commands:
+
+```
+npm install
+```
+
+
+## Usage
+
+- When user clicks on the delete icon next to note, that note is deleted and removed from list.
+![Delete note](./Asset/delete-note.png)
+
+## License
+
+      Copyright © MIT. All rights reserved. 
+      
+      Licensed under the MIT license.
+
+
+## Contributing
+To make this project better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+
+* Fork the Project
+* Create your Feature Branch (git checkout -b feature/AmazingFeature)
+* Commit your Changes (git commit -m 'Add some AmazingFeature')
+* Push to the Branch (git push origin feature/AmazingFeature)
+* Open a Pull Request
+
+
+## Tests
+
+To run tests, run these commands:
+
+```
+npm run test
+```
+
+## Questions
+This project is contributed by [yul402](https://github.com/yul402/)
+
+Contact infotmation: Ying.li20@outlook.com
--- index.js
+++ index.js
@@ -1,78 +1,74 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { Shape,Triangle, Square, Circle } = require('./lib/shapes');
+const { Shape, Triangle, Square, Circle } = require('./lib/shapes');
 
 
 
 // Prompt the user questions and save user input
 function promptUser() {
-    inquirer
-      .prompt([
-        //Logo text
-        {
-          type: "input",
-          message:"What text would you like to put in logo (up to three characters)?",
-          name: "text",
-          validate: function(text)
-                {
-                    // Check input text length
-                    const textLen = text.length;
-                    if (textLen > 3) {
-                        console.log("\nLogo text should ne no more than 3 characters")
-                        return false;
-                      } else {
-                          return true;}
-                    }
-        },
-        // Logo text color prompt
-        {
-          type: "input",
-          message:
-            "Choose text color (Enter color keyword OR a hexadecimal number)",
-          name: "textColor",
-        },
-        // Choose logo shape
-        {
-          type: "list",
-          message: "What shape would you like the logo to be?",
-          choices: ["Triangle", "Square", "Circle"],
-          name: "shape",
-        },
-        // Choose shape color
-        {
-          type: "input",
-          message:
-            "Choose shapes color (Enter color keyword OR a hexadecimal number)",
-          name: "shapeColor",
-        },
+  inquirer
+    .prompt([
+      //Logo text
+      {
+        type: "input",
+        message: "What text would you like to put in logo (up to three characters)?",
+        name: "text",
+        validate: function (text) {
+          // Check input text length
+          const textLen = text.length;
+          if (textLen > 3) {
+            console.log("\nLogo text should ne no more than 3 characters")
+            return false;
+          } else {
+            return true;
+          }
+        }
+      },
+      // Logo text color prompt
+      {
+        type: "input",
+        message:
+          "Choose text color (Enter color keyword OR a hexadecimal number)",
+        name: "textColor",
+      },
+      // Choose logo shape
+      {
+        type: "list",
+        message: "What shape would you like the logo to be?",
+        choices: ["Triangle", "Square", "Circle"],
+        name: "shape",
+      },
+      // Choose shape color
+      {
+        type: "input",
+        message:
+          "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+        name: "shapeColor",
+      },
     ]).then((answers) => {
-          // Calling write file function to generate SVG file
-          writeToFile("./examples/logo.svg", answers);
-      });
-    }
+      // Calling write file function to generate SVG file
+      writeToFile( answers);
+    });
+}
 
 // Generate logo.svg in "examples" folder
-function writeToFile(filePath,userAnswer){
-  switch(userAnswer.shape) {
+function writeToFile( userAnswer) {
+  let userShape;
+  switch (userAnswer.shape) {
     case 'Triangle':
-      const triangleContent = new Triangle(userAnswer)
-      fs.writeFile(filePath,triangleContent.render(), (err) => {
-        err ? console.log(err) : console.log("Generated logo.svg");
-      });
+      userShape = new Triangle(userAnswer)
       break
     case 'Square':
-      const squareContent = new Square(userAnswer)
-      fs.writeFile(filePath,squareContent.render(), (err) => {
-        err ? console.log(err) : console.log("Generated logo.svg");
-      })
+     userShape = new Square(userAnswer)
       break
     case 'Circle':
-      const circleContent = new Circle(userAnswer)
-      fs.writeFile(filePath,circleContent.render(), (err) => {
-        err ? console.log(err) : console.log("Generated logo.svg");
-      });
+      userShape = new Circle(userAnswer)
       break
   }
+
+  fs.writeFile('./dist/logo.svg', userShape.render(), (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg");
+  });
 }
 
 
